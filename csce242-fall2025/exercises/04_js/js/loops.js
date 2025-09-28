48f4c756fac734e57dfbfc932dbc0c6962bdcd62
dylancobb2525 <dylancobb2525@gmail.com>
2025-09-28 14:11:28 -0400

    made all edits recommended by my peers along with Miss Plante

--- csce242-fall2025/exercises/04_js/js/loops.js
+++ csce242-fall2025/exercises/04_js/js/loops.js
@@ -24,4 +24,49 @@ document.getElementById("btn-count").onclick = () => {
         error.innerHTML = "* second number must be greater than first number";
         return;
     }
+    //loop for every number from start to end display the number in paragraphs
+    for(let i = startNum; i <= endNum; i++) {
+        const p = document.createElement("p");
+        p.innerHTML = i;
+        resultDiv.append(p);
+        p.onclick = () => {
+            console.log(`You clicked on ${i}`);
+        }
+    }
+};
+
+document.getElementById("btn-show-toys").onclick = () => {
+    const toyList = document.getElementById("toy-list");
+    toyList.innerHTML = "";
+    
+    const toys = ["ball", "rocket", "spaceship", "robot", "bear"];
+
+    toys.forEach((toy, i) => {
+        const li = document.createElement("li");
+        li.innerHTML = toy;
+        toyList.append(li);
+    });
+}
+
+document.getElementById("btn-show-toy-prices").onclick = () => {
+    const toys = [];
+    toys["ball"] = 2.99;
+    toys["rocket"] = 10.99;
+    toys["spaceship"] = 15.99;
+    toys["robot"] = 20.99;
+    toys["bear"] = 25.99;
+
+    const toyDiv = document.getElementById("toy-prices");
+    toyDiv.innerHTML = "";
+
+    for(let toy in toys) {
+        const p = document.createElement("p");
+        p.innerHTML = toy;
+        toyDiv.append(p);
+
+        p.onclick = () => {
+            priceP.innerHTML = `${toy}: $${toys[toy]}`;
+        };
+    }  
+
 };
